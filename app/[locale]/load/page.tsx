80eada1f8d5273410c72fb32907c5d222bda2d0c
Caleb Ganza <icaleb130@gmail.com>
2025-05-17 20:21:42 +0200

    refactor: Changing the colors for the loading skeleton

---
+++ app/[locale]/load/page.tsx
@@ -0,0 +1,8 @@
+import { ComplaintFormSkeleton } from "@/components/ComplaintFormSkeleton";
+import React from "react";
+
+const Load = () => {
+  return <ComplaintFormSkeleton />;
+};
+
+export default Load;
