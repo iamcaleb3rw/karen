87c1f09f99b7c1f4e6da55c5c68e773aa590e585
Caleb Ganza <icaleb130@gmail.com>
2025-05-17 22:55:30 +0200

    feat: Adding translation on the Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,15 @@
+"use client";
 import React, { Suspense } from "react";
 import Logo from "@/public/logo.svg";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./ui/button";
-import { Search } from "lucide-react";
+import { LogIn, Search, UserPlus } from "lucide-react";
 import { LanguageSwitch } from "./LanguageSwitch";
+import { useTranslations } from "next-intl";
 
 const Navbar = () => {
+  const t = useTranslations("HomePage");
   return (
     <nav className="text-white px-6 py-2 flex items-center justify-between">
       <div className="flex items-center">
@@ -14,11 +17,21 @@ const Navbar = () => {
         <p className="text-lg font-semibold">KarenCMS</p>
       </div>
       <div className="flex items-center gap-6">
+        <div className="flex gap-8 text-sm">
+          <Link href={"/login"} className="flex items-center gap-1">
+            <LogIn />
+            {t("Navigation.auth.login")}
+          </Link>
+          <Link href={"/signup"} className="flex items-center gap-1">
+            <UserPlus />
+            {t("Navigation.auth.signup")}
+          </Link>
+        </div>
         <div>
           <Link href="/searchdoc">
             <Button className="bg-muted/30 text-white hover:bg-muted/40 cursor-pointer">
               <Search />
-              Search complaints
+              {t("Navigation.search.text")}
             </Button>
           </Link>
         </div>
